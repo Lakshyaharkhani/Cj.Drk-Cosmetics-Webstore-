be820b94c59ffb68ef1ecb82995d7fd133cbc468
Lakshya Harkhani <lakshyaharkhani@gmail.com>
2025-12-04 06:03:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "cjdrkcosmeticstore",
+  "appId": "1:705918177860:web:24a47f07f43335348d2211",
+  "apiKey": "AIzaSyAK-60WrdrXud8RdIuWEpBdzpXi4ELYVxc",
+  "authDomain": "cjdrkcosmeticstore.firebaseapp.com",
+  "measurementId": "G-N043TM49HT",
+  "messagingSenderId": "705918177860"
+};
