6b6cbe822eb8f350f73f65d24dfafea24f5901db
Lakshya Harkhani <lakshyaharkhani@gmail.com>
2025-12-04 04:57:08 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children,
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
