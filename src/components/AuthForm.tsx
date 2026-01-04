7f5a1c0414664148e9de8d971c76f13ada908487
Lakshya Harkhani <lakshyaharkhani@gmail.com>
2026-01-04 07:42:24 +0000

    there ios alredy a login/sign ip page kindly use that or use this auth.t

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -6,7 +6,7 @@ import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
-import { useAuth, useUser } from '@/firebase';
+import { useAuth } from '@/firebase';
 import { initiateEmailSignIn, initiateEmailSignUp } from '@/firebase/non-blocking-login';
 import { useToast } from '@/hooks/use-toast';
 
